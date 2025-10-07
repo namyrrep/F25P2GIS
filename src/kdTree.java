b0bc8e5e25afb7271cd9478d415d65b610bedb96
namyrrep <namyrrep@vt.edu>
2025-10-07 14:36:09 -0400

    Update kdTree.java

--- src/kdTree.java
+++ src/kdTree.java
@@ -1,29 +1,49 @@
 /**
  * 
  */
-
-/**
- * 
- */
 public class kdTree {
     private BinaryNode<City> root;
-    
+
+    /**
+     * Initializes a new tree
+     * 
+     * @param currentCity
+     */
     public kdTree(City currentCity) {
         root = new BinaryNode<City>(currentCity);
     }
-    
+
+
+    /**
+     * Gets the current root
+     * 
+     * @return BinaryNode<City> returns the node
+     */
     public BinaryNode<City> getRoot() {
         return root;
     }
-    
-    public boolean insertNode(BinaryNode<City> node) {
-        
+
+
+    /**
+     * Insert function to add to the tree
+     * 
+     * @param city
+     * @return boolean if successfully inserted
+     */
+    public boolean insert(City city) {
+
         return true;
     }
-    
+
+
+    /**
+     * Returns the tree printed out
+     * 
+     * @return String of the tree
+     */
     public String toString() {
-        
+
         return "";
     }
-    
+
 }
