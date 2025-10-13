860801f36337b19f7336705d2f27f2880fe938b7
namyrrep <namyrrep@vt.edu>
2025-10-12 21:17:43 -0400

    Add tests for info and delete methods in GISTest
    Added assertions to test the info method with coordinates and the delete method with a city name. These changes improve coverage and ensure correct behavior for these functionalities.

--- src/GISTest.java
+++ src/GISTest.java
@@ -82,13 +82,14 @@ public class GISTest extends TestCase {
             + "2    L (11, 500)\n" + "0Chicago (100, 150)\n"
             + "1  Tacoma (1000, 100)\n" + "2    L (101, 150)\n", it.debug());
         assertFuzzyEquals("L (101, 150)\nL (11, 500)", it.info("L"));
-
+        assertFuzzyEquals("L", it.info(101, 150));
+        assertFuzzyEquals("Tacoma (1000, 100)", it.delete("Tacoma"));
         it.clear();
 
         /**
          * 
-         * assertFuzzyEquals("L", it.info(101, 150));
-         * assertFuzzyEquals("Tacoma (1000, 100)", it.delete("Tacoma"));
+         * 
+         * 
          * assertFuzzyEquals("3\nChicago", it.delete(100, 150));
          * assertFuzzyEquals("L (101, 150)\n" + "Atlanta (10, 500)\n"
          * + "Baltimore (0, 300)\n" + "Washington (5, 350)\n"
